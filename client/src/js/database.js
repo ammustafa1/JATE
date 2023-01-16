59417899cf54306c1748be078afdf002f1091c6e
ammustafa1 <109623721+ammustafa1@users.noreply.github.com>
2023-01-15 21:35:55 -0500

    Add files via upload

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const DB_NAME = 'jate';
+  const DB_VERSION = 1;
+  
+  const openDatabaseConnection = async () => {
+    return await window.indexedDB.open(DB_NAME, DB_VERSION);
+  }
+  
+  export const storeData = async (content) => {
+    const db = await openDatabaseConnection();
+    const tx = db.transaction('jate', 'readwrite');
+    const jateStore = tx.objectStore('jate');
+    const addDataReq = jateStore.put({ id: 1, content });
+    const result = await addDataReq;
+  }
+  
+  export const fetchData = async () => {
+    const db = await openDatabaseConnection();
+    const tx = db.transaction('jate', 'readonly');
+    const jateStore = tx.objectStore('jate');
+    const fetchDataReq = jateStore.get(1);
+    const result = await fetchDataReq;
+    console.log(result);
+    return result?.content;
+  }
+  
 
 initdb();
